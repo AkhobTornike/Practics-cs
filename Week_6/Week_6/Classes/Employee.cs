88650aead386f7091b39d847f42cd5e37480bf13
AkhobTornike <105112313+AkhobTornike@users.noreply.github.com>
2023-11-17 16:37:40 +0400

    laboratory 5 i have pload new proect named week6 there is code for task laboratory 5

---
+++ Week_6/Week_6/Classes/Employee.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Week_6.Classes
+{
+    internal class Employee : IEnumerable<Employee>, IEnumerator<Employee>, IComparable<Employee>, IComparer<Employee>, IEquatable<Employee>
+    {
+        private List<Employee> employees;
+        private int position = -1;
+
+        public string firt_name { get; set; }
+        public string last_name { get; set; }
+        public string id { get; set; }
+        public string birthday { get; set; }
+        public int age => CalculateAge();
+        public string salary { get; set; }
+        public string reiting { get; set; }
+
+        public Employee()
+        {
+            employees = new List<Employee>();
+        }
+
+        public Employee(string firt_name, string last_name, string id, string birthday, string salary, string reiting, List<Employee> employees, int position)
+        {
+            this.firt_name = firt_name;
+            this.last_name = last_name;
+            this.id = id;
+            this.birthday = birthday;
+            this.salary = salary;
+            this.reiting = reiting;
+            this.employees = employees;
+            this.position = position;
+        }
+
+        public IEnumerator<Employee> GetEnumerator()
+        {
+            return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public Employee Current => employees[position];
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+            // Implement if needed
+        }
+
+        public bool MoveNext()
+        {
+            position++;
+            return position < employees.Count;
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        public int CompareTo(Employee other)
+        {
+            return this.reiting.CompareTo(other.reiting);
+        }
+
+        public int Compare(Employee x, Employee y)
+        {
+            return x.salary.CompareTo(y.salary);
+        }
+
+        public bool Equals(Employee other)
+        {
+            return this.id.Equals(other.id);
+        }
+
+        private int CalculateAge()
+        {
+            return DateTime.Now.Year - int.Parse(birthday);
+        }
+    }
+}
---
+++ Week_6/Week_6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Week_6.Classes;
+
+namespace Week_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employeeList = new List<Employee>();
+
+            Employee employee1 = new Employee("John", "Doe", "123456789", "1990-05-15", "50000", "4", employeeList, 0);
+            Employee employee2 = new Employee("Jane", "Smith", "987654321", "1985-08-22", "60000", "5", employeeList, 0);
+            Employee employee3 = new Employee("Bob", "Johnson", "111223344", "1988-03-10", "55000", "3", employeeList, 0);
+
+            employeeList.Add(employee1);
+            employeeList.Add(employee2);
+            employeeList.Add(employee3);
+
+            employeeList.Sort(employee1);
+
+            employeeList.Sort(employee2);
+
+            employeeList.Sort(employee3);
+
+            foreach (Employee employee in employeeList)
+            {
+                Console.WriteLine($"{employee.firt_name} {employee.last_name} - Rating: {employee.reiting}, Salary: {employee.salary}");
+            }
+        }
+    }
+}
